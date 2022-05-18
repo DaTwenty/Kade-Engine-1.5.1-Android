0b79b83c5792a1680ea2dbceaf4c4e6d42a8c3a9
DaTwenty <94628507+DaTwenty@users.noreply.github.com>
2022-05-18 01:53:07 -0300

    Add files via upload

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -51,7 +51,7 @@ class FreeplayState extends MusicBeatState
 			if (FlxG.sound.music != null)
 			{
 				if (!FlxG.sound.music.playing)
-					FlxG.sound.playMusic(Paths.music('freakyMenu'));
+					FlxG.sound.playMusic(Paths.music('freakyMenuHex'));
 			}
 
 
